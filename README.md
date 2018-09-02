3b27eea9dbfa61623c9616f157930e748edb4ca2
Amit Dimri <amitad16jan@gmail.com>
2018-09-02 23:33:01 +0530

    converted to plugin

---
+++ README.md
@@ -0,0 +1,70 @@
+# StirngRoller.js
+
+### Demo
+
+[https://amitad16.github.io/StringChangeStyle/](https://amitad16.github.io/StringChangeStyle/)
+
+### Dependencies
+
+No dependacies! Written in plain javascript.
+
+### Syntax
+
+```js
+StringChange(selector, { finalText: Some string }).play();
+```
+
+### Use
+
+```html
+<p id='stringRoller'>What is this?</p>
+<button type='button' id='roll'>Roll It</button>
+```
+
+```js
+window.onload = () => {
+  let rollBtn = document.querySelector("button#roll");
+  rollBtn.addEventListener("click", () => {
+    StringChange("#stringRoller", {
+      finalText: "This is StringRoller.js"
+    }).play();
+  });
+};
+```
+
+### Full Example
+
+```html
+<html>
+<head>
+	<title>StringRoller.js</title>
+</head>
+<body>
+	<p id='stringRoller'>What is this?</p>
+	<button type='button' id='roll'>Roll It</button>
+
+	<script src="./src/stringRoller.js"></script>
+	<script>
+		window.onload = () => {
+			let rollBtn = document.querySelector("button#roll");
+			rollBtn.addEventListener("click", () => {
+				StringRoller('#stringRoller', { finalText: "This is StringRoller.js" }).roll();
+			});
+		}
+	</script>
+</body>
+</html>
+```
+
+### Options
+
+```js
+{
+  // Mandatory options
+  finalText: 'Some text', // String to change to
+}
+```
+
+### Licence
+
+Licensed under the MIT license.
---
+++ src/stringRoller.js
@@ -0,0 +1,70 @@
+let StringRoller = (selector, options) => {
+  "use strict";
+
+  let self = {};
+
+  self.container = document.querySelector(selector);
+  self.options = {
+    initialText: self.container.firstChild.nodeValue,
+    finalText: options.finalText
+  };
+
+  self.roll = () => {
+    let i = 0,
+      totalTime = 500,
+      finalCharTime = 600,
+      addCharTime = !!self.options.finalText
+        ? Math.floor(totalTime / self.options.finalText.length)
+        : 50,
+      changeCharTime = !!self.options.finalText
+        ? totalTime / (self.options.finalText.length * 2)
+        : 25;
+
+    self.container.innerHTML = ".";
+
+    let replaceAt = function(index, replacement) {
+      return (
+        this.substr(0, index) +
+        replacement +
+        this.substr(index + replacement.length)
+      );
+    };
+
+    let manageStringState = index => {
+      let changeVal = setInterval(() => {
+        let random = Math.random()
+          .toString(32)
+          .substr(2, 1);
+        self.container.innerHTML = replaceAt.call(
+          self.container.firstChild.nodeValue,
+          index,
+          random
+        );
+      }, changeCharTime);
+
+      setTimeout(() => {
+        clearInterval(changeVal);
+        self.container.innerHTML = replaceAt.call(
+          self.container.firstChild.nodeValue,
+          index,
+          self.options.finalText[index]
+        );
+      }, finalCharTime);
+    };
+
+    // Initial run
+    let add = setInterval(() => {
+      if (!self.options.finalText) {
+        self.container.innerHTML = self.options.initialText;
+        clearInterval(add);
+      } else if (!self.options.finalText[i]) {
+        clearInterval(add);
+      } else {
+        manageStringState(i);
+        i++;
+      }
+    }, addCharTime);
+  };
+
+  return self;
+};
